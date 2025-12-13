3956f193833446d8d6689cc0365aca26f3363866
sagnikBag <146710246+Sagnikrick568@users.noreply.github.com>
2025-12-14 02:13:05 +0530

    shopcart

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,14 @@ import Order from './pages/Order'
 import Navbar from './component/Navbar'
 import Footer from './component/Footer'
 import SearchBar from './component/SearchBar'
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
       <Navbar/>
       <SearchBar/>
       <Routes>
